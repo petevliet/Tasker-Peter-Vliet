d5600bb604466c2dad7c23edad4afc67acfbf8ae
Peter Vliet <petervliet@gmail.com>
2015-01-21 15:40:59 -0700

    remove turbolink from gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'about' => 'welcome#about'
+  get 'faq' => 'faq#show'
+  get 'terms' => 'welcome#terms'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
