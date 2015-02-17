cd6085675db5763bdb2aadcaf743b8aa372abe33
Peter Vliet <petervliet@gmail.com>
2015-02-17 16:26:19 -0700

    user cannot create/edit user if one of the fields is empty

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+
   def fullname
     "#{first_name} #{last_name}"
   end
