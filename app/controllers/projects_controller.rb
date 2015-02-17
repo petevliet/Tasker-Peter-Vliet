efb71447eb0f83fdffa2b9d89ed218d79a50c5ba
Peter Vliet <petervliet@gmail.com>
2015-02-17 16:05:43 -0700

    user will see error message if project does not have name

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -32,7 +32,7 @@ class ProjectsController < ApplicationController
       if @project.update(project_params)
         redirect_to @project, notice: 'Project was successfully updated.'
       else
-        render :new
+        render :edit
       end
   end
 
