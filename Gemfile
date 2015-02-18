aec9c84ae49b60d7b196d355c65398f9ae8071c2
Peter Vliet <petervliet@gmail.com>
2015-02-18 13:48:51 -0700

    add signup/in to toolbar, uncomment bcrypt, edit user model

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'pg'
 gem 'pry-rails'
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
 
+  has_secure_password
+
   def fullname
     "#{first_name} #{last_name}"
   end
