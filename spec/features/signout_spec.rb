985fdefb7166b922633432da17260b3da9498151
Peter Vliet <petervliet@gmail.com>
2015-03-02 15:48:43 -0700

    cleanup markup from views

--- spec/features/signout_spec.rb
+++ spec/features/signout_spec.rb
@@ -4,16 +4,16 @@ describe 'user can sign out' do
 
   before :each do
     Registration.create(first_name: 'joe', last_name: 'camel', email: 'joe@camel.com', password: 'password')
+  end
+
+  it 'user can sign out' do
 
     visit '/signin'
 
     fill_in 'email', with: 'joe@camel.com'
     fill_in 'password', with: 'password'
     click_button 'Sign In'
-  end
 
-
-  it 'user can sign out' do
     visit '/'
     click_on 'Sign Out'
 
