250d992bcea28c831511528eb4005f11c6ea3402
Peter Vliet <petervliet@gmail.com>
2015-03-10 16:53:15 -0600

    users memberships can be updated on project membership index

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -18,6 +18,16 @@ class MembershipsController < ApplicationController
     end
   end
 
+  def update
+    @project = Project.find(params[:project_id])
+    @membership = Membership.find(params[:id])
+    if @membership.update(membership_params)
+      redirect_to project_memberships_path(@project), notice: "#{@membership.user.first_name} was successfully updated"
+    else
+      render :index
+    end
+  end
+
   def membership_params
     params.require(:membership).permit(:user_id, :project_id, :role)
   end
