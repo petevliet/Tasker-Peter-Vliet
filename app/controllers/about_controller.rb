0dca1f4a343cbebed9104a369706edfada046aeb
Peter Vliet <petervliet@gmail.com>
2015-04-07 15:50:23 -0600

    users can see data counts on about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,9 @@
 class AboutController < ApplicationController
   def index
+    @projects = Project.all.count
+    @tasks = Task.all.count
+    @memberships = Membership.all.count
+    @users = User.all.count
+    @comments = Comment.all.count
   end
 end
