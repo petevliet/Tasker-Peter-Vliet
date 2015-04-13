42e006f3aff48ebc4abea510c4146f7796579cec
Peter Vliet <petervliet@gmail.com>
2015-04-13 15:39:58 -0600

    specs for marketing and internal pages

---
+++ spec/features/internal_pages_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'internal pages have a different layout than marketing pages' do
+
+  before :each do
+    User.create(id: 1, first_name: 'joe', last_name: 'schmo', email: 'joe@schmo.com', password: 'password')
+    Project.create(id: 1, name: 'example')
+    Membership.create(id: 1, user_id: 1, project_id: 1, role: 1)
+
+    visit '/'
+    click_on 'Sign In'
+    fill_in 'email', with: 'joe@schmo.com'
+    fill_in 'password', with: 'password'
+    click_button 'Sign In'
+  end
+
+  it 'gCamp on navbar links to projects for signed in users' do
+    click_on 'gCamp'
+    expect(current_path).to eq(projects_path)
+  end
+
+  it 'signed in user sees Project dropdown' do
+    within('li.dropdown') {click_on 'example'}
+    expect(page).to have_content 'Deleting this project'
+  end
+
+  it 'signed in user sees Users link in navbar' do
+    click_link('users_link')
+    expect(page).to have_content 'New user'
+  end
+
+
+end
---
+++ spec/features/loggedout_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'logged out users cannot see marketing pages' do
+
+  before :each do
+    Project.create(id: 1, name: 'testing')
+  end
+
+  it 'gCamp link on navbar links to root path' do
+    visit '/'
+    click_on 'gCamp'
+    expect(current_path).to eq(root_path)
+  end
+
+  it 'signed out users cannot see projects' do
+    visit '/projects'
+    expect(page).to have_content 'You must be logged in'
+  end
+
+  it 'signed out users cannot see tasks' do
+    visit '/projects/1'
+
+    expect(page).to have_content 'You must be logged in'
+  end
+
+  it 'signed out users cannot see memberships' do
+    visit '/projects/1'
+
+    expect(page).to have_content 'You must be logged in'
+  end
+
+  it 'signed out users cannot see users' do
+
+    visit '/users'
+    expect(page).to have_content 'You must be logged in'
+  end
+
+end
